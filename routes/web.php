a684acb11999bdc7fd236425675c6f74dc93a01c
rickysut <ricky.sutanto@gmail.com>
2022-07-14 03:11:51 +0000

    test change

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//changed
+
 
 Route::get('/', function () {
     return view('welcome');
+
 });
